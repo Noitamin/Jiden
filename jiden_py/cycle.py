da04cbf695f49c0a7fb364957307d24b0a122360
Noitamin <genesis-ii@hotmail.com>
2017-01-09 01:51:39 +0000

    Modified cycle.py to use designated GPIO pins Added startup script to set GPIO pin direction to OUT

--- jiden_py/cycle.py
+++ jiden_py/cycle.py
@@ -5,13 +5,14 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 # init list with pin numbers
-pinList = [9, 10, 22, 27, 17, 4, 3, 2]
+pinList = [6, 12, 13, 16, 19, 20, 26, 21]
+pinList = [12, 6, 16, 13, 20, 19, 21, 26]
 
 # loop through pins and set mode and state to 'low'
 
 for i in pinList: 
     GPIO.setup(i, GPIO.OUT) 
-    GPIO.output(i, GPIO.HIGH)
+    #GPIO.output(i, GPIO.LOw)
 
 # time to sleep between operations in the main loop
 
@@ -22,15 +23,15 @@ SleepTimeL = 1
 try:
   print "Started: cycle.py"
   for j in pinList: 
-    GPIO.output(j, GPIO.LOW)
-    print "  GPIO " + str(j) + " is LOW (On)"
-    time.sleep(SleepTimeL)
-
     GPIO.output(j, GPIO.HIGH)
-    print "  GPIO " + str(j) + " is HIGH (Off)"
+    print "  GPIO " + str(j) + " is HIGH (ON)"
     time.sleep(SleepTimeL);
 
-  GPIO.cleanup()
+    GPIO.output(j, GPIO.LOW)
+    print "  GPIO " + str(j) + " is LOW (OFF)"
+    time.sleep(SleepTimeL)
+
+  #GPIO.cleanup()
   print "Finished: cycle.py"
 
 # End program cleanly with keyboard
@@ -38,4 +39,4 @@ except KeyboardInterrupt:
   print "Good Bye"
 
   # Reset GPIO settings
-  GPIO.cleanup()
+  #GPIO.cleanup()
